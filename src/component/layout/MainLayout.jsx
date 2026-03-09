28dc9a7adf2440739edb55b7f940cae79f9fb43c
Tausif Alam <tausifalam87654321@gmail.com>
2026-03-09 11:47:12 +0530

    fix: add ScrollToTop for proper page navigation

--- src/component/layout/MainLayout.jsx
+++ src/component/layout/MainLayout.jsx
@@ -3,10 +3,12 @@ import NavBar from '../common/NavBar'
 import { Outlet } from 'react-router-dom'
 import Footer from '../common/Footer'
 import WhatsAppButton from '../common/WhatsAppButton'
+import ScrollToTop from '../common/ScrollToTop'
 
 const MainLayout = () => {
   return (
     <>
+    <ScrollToTop />
     <NavBar />
     <main className='min-h-screen'>
         <Outlet />
